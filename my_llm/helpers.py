5ac0f8d869b55715f399ce456199a7544f5e4e85
oscardef <o.m.de.francesca@student.rug.nl>
2025-04-06 23:17:18 +0200

    training code

--- my_llm/helpers.py
+++ my_llm/helpers.py
@@ -12,6 +12,61 @@ class GELU(nn.Module):
         ))
     
 
+def text_to_token_ids(text, tokenizer):
+        encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
+        encoded_tensor = torch.tensor(encoded).unsqueeze(0) # add batch dimension
+        return encoded_tensor
+
+def token_ids_to_text(token_ids, tokenizer):
+    """
+    Convert token IDs to text.
+    Args:
+        token_ids: Tensor of token IDs.
+        tokenizer: Tokenizer to decode the token IDs.
+    Returns:
+        decoded_text: Decoded text.
+    """
+    flat = token_ids.squeeze(0) # remove batch dimension
+    return tokenizer.decode(flat.tolist())
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    """
+    Generate text using the model.
+    Args:
+        model: The model to be used for generation.
+        idx: Tensor of token IDs.
+        max_new_tokens: Maximum number of new tokens to generate.
+        context_size: Size of the context window.
+    Returns:
+        idx: Tensor of generated token IDs.
+    """
+    # idx is (batch, n_tokens) array of indices in the current context
+    for _ in range(max_new_tokens):
+        
+        # Crop current context if it exceeds the supported context size
+        # E.g., if LLM supports only 5 tokens, and the context size is 10
+        # then only the last 5 tokens are used as context
+        idx_cond = idx[:, -context_size:]
+        
+        # Get the predictions
+        with torch.no_grad():
+            logits = model(idx_cond)
+        
+        # Focus only on the last time step
+        # (batch, n_tokens, vocab_size) becomes (batch, vocab_size)
+        logits = logits[:, -1, :]  
+
+        # Apply softmax to get probabilities
+        probas = torch.softmax(logits, dim=-1)  # (batch, vocab_size); normalize along vocab dim
+
+        # Get the idx of the vocab entry with the highest probability value
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)  # (batch, 1)
+
+        # Append sampled index to the running sequence
+        idx = torch.cat((idx, idx_next), dim=1)  # (batch, n_tokens+1)
+
+    return idx
+
 def calc_loss_batch(input_batch, target_batch, model, device):
     """
     Calculate the loss for a single batch of data.
--- my_llm/model.py
+++ my_llm/model.py
@@ -28,44 +28,6 @@ class GPTModel(nn.Module):
         x = self.final_norm(x) # Apply layer normalization
         logits = self.out_head(x) # Get the logits
         return logits # Shape [batch_size, num_tokens, vocab_size]
-    
-    def text_to_token_ids(text, tokenizer):
-        encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
-        encoded_tensor = torch.tensor(encoded).unsqueeze(0) # add batch dimension
-        return encoded_tensor
-
-    def token_ids_to_text(token_ids, tokenizer):
-        flat = token_ids.squeeze(0) # remove batch dimension
-        return tokenizer.decode(flat.tolist())
-
-    def generate_text_simple(model, idx, max_new_tokens, context_size):
-        # idx is (batch, n_tokens) array of indices in the current context
-        for _ in range(max_new_tokens):
-            
-            # Crop current context if it exceeds the supported context size
-            # E.g., if LLM supports only 5 tokens, and the context size is 10
-            # then only the last 5 tokens are used as context
-            idx_cond = idx[:, -context_size:]
-            
-            # Get the predictions
-            with torch.no_grad():
-                logits = model(idx_cond)
-            
-            # Focus only on the last time step
-            # (batch, n_tokens, vocab_size) becomes (batch, vocab_size)
-            logits = logits[:, -1, :]  
-
-            # Apply softmax to get probabilities
-            probas = torch.softmax(logits, dim=-1)  # (batch, vocab_size); normalize along vocab dim
-
-            # Get the idx of the vocab entry with the highest probability value
-            idx_next = torch.argmax(probas, dim=-1, keepdim=True)  # (batch, 1)
-
-            # Append sampled index to the running sequence
-            idx = torch.cat((idx, idx_next), dim=1)  # (batch, n_tokens+1)
-
-        return idx
-
 
 class TransformerBlock(nn.Module):
     def __init__(self, cfg):
