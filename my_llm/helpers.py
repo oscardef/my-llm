a64ac2b701f84de024fadc441bf84e551eb78f12
oscardef <o.m.de.francesca@student.rug.nl>
2025-04-06 23:07:58 +0200

    halfway through chapter 5 (training)

--- my_llm/helpers.py
+++ my_llm/helpers.py
@@ -10,3 +10,49 @@ class GELU(nn.Module):
             torch.sqrt(torch.tensor(2.0 / torch.pi)) * 
             (x + 0.044715 * torch.pow(x, 3))
         ))
+    
+
+def calc_loss_batch(input_batch, target_batch, model, device):
+    """
+    Calculate the loss for a single batch of data.
+    Args:
+        input_batch: Input data batch.
+        target_batch: Target data batch.
+        model: The model to be evaluated.
+        device: The device to perform the computation on.
+    Returns:
+        loss: The calculated loss.
+    """
+    input_batch, target_batch = input_batch.to(device), target_batch.to(device)
+    logits = model(input_batch)
+    loss = torch.nn.functional.cross_entropy(logits.flatten(0, 1), target_batch.flatten())
+    return loss
+
+
+def calc_loss_loader(data_loader, model, device, num_batches=None):
+    """
+    Calculate the average loss over a data loader.
+    Args:
+        data_loader: Data loader containing the dataset.
+        model: The model to be evaluated.
+        device: The device to perform the computation on.
+        num_batches: Number of batches to evaluate. If None, evaluates all batches.
+    Returns:
+        average_loss: The average loss over the specified number of batches.
+    """
+    total_loss = 0.
+    if len(data_loader) == 0:
+        return float("nan")
+    elif num_batches is None:
+        num_batches = len(data_loader)
+    else:
+        # Reduce the number of batches to match the total number of batches in the data loader
+        # if num_batches exceeds the number of batches in the data loader
+        num_batches = min(num_batches, len(data_loader))
+    for i, (input_batch, target_batch) in enumerate(data_loader):
+        if i < num_batches:
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            total_loss += loss.item()
+        else:
+            break
+    return total_loss / num_batches # Average loss over the specified number of batches
--- my_llm/model.py
+++ my_llm/model.py
@@ -21,13 +21,50 @@ class GPTModel(nn.Module):
     def forward(self, in_idx):
         batch_size, seq_len = in_idx.shape # Get the batch size and sequence length
         tok_embeds = self.tok_emb(in_idx) # Get the token embeddings
-        pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device)) # Get the position embeddings
+        pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device)) # Arange will create a tensor with values from 0 to seq_len. Device will put the tensor on the same device as in_idx
         x = tok_embeds + pos_embeds  # Shape [batch_size, num_tokens, emb_size]
         x = self.drop_emb(x) # Apply dropout to the embeddings
         x = self.trf_blocks(x) # Apply the transformer blocks
         x = self.final_norm(x) # Apply layer normalization
         logits = self.out_head(x) # Get the logits
         return logits # Shape [batch_size, num_tokens, vocab_size]
+    
+    def text_to_token_ids(text, tokenizer):
+        encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
+        encoded_tensor = torch.tensor(encoded).unsqueeze(0) # add batch dimension
+        return encoded_tensor
+
+    def token_ids_to_text(token_ids, tokenizer):
+        flat = token_ids.squeeze(0) # remove batch dimension
+        return tokenizer.decode(flat.tolist())
+
+    def generate_text_simple(model, idx, max_new_tokens, context_size):
+        # idx is (batch, n_tokens) array of indices in the current context
+        for _ in range(max_new_tokens):
+            
+            # Crop current context if it exceeds the supported context size
+            # E.g., if LLM supports only 5 tokens, and the context size is 10
+            # then only the last 5 tokens are used as context
+            idx_cond = idx[:, -context_size:]
+            
+            # Get the predictions
+            with torch.no_grad():
+                logits = model(idx_cond)
+            
+            # Focus only on the last time step
+            # (batch, n_tokens, vocab_size) becomes (batch, vocab_size)
+            logits = logits[:, -1, :]  
+
+            # Apply softmax to get probabilities
+            probas = torch.softmax(logits, dim=-1)  # (batch, vocab_size); normalize along vocab dim
+
+            # Get the idx of the vocab entry with the highest probability value
+            idx_next = torch.argmax(probas, dim=-1, keepdim=True)  # (batch, 1)
+
+            # Append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1)  # (batch, n_tokens+1)
+
+        return idx
 
 
 class TransformerBlock(nn.Module):
@@ -113,7 +150,9 @@ torch.manual_seed(123)
 
 from config import GPT_CONFIG_124M
 
-model = GPTModel(cfg=GPT_CONFIG_124M)
-logits = model(batch)
-print("Output shape:", logits.shape)
-print(logits)
+model = GPTModel(GPT_CONFIG_124M)
+
+out = model(batch)
+print("Input batch:\n", batch)
+print("\nOutput shape:", out.shape)
+print(out)
